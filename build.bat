812556c9f79a9e1ae516d0f98e22b701b600569b
arttu <arttuc@pm.me>
2025-01-06 18:47:01 +0200

    Build script fixes
    - Add all pages as hidden imports
    - Rename LICENSE to LICENSE.txt for windows build

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-pyinstaller -F Main.py --noconsole --hidden-import yt_dlp
-copy "LICENSE" "dist\LICENSE"
+pyinstaller -F Main.py --noconsole --name YTMediaTool --hidden-import yt_dlp --hidden-import BasicPage --hidden-import SMLDpage --hidden-import SettingsPage --hidden-import AboutPage
+copy "LICENSE" "dist\LICENSE.txt"
 explorer dist
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-pyinstaller -F Main.py --noconsole --hidden-import yt_dlp
+pyinstaller -F Main.py --noconsole --name YTMediaTool --hidden-import yt_dlp --hidden-import BasicPage --hidden-import SMLDpage --hidden-import SettingsPage --hidden-import AboutPage
 cp LICENSE dist/LICENSE # copy license to distribution
 xdg-open dist
