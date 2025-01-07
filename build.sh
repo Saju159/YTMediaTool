78153cfbbe4ae531230b52bf3b7bd9d6225907b0
Arttu <arttuc@pm.me>
2025-01-07 19:03:40 +0200

    Adjust linux build script to work with new path

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-pyinstaller -F Main.py --noconsole --name YTMediaTool --hidden-import yt_dlp --hidden-import BasicPage --hidden-import SMLDpage --hidden-import SettingsPage --hidden-import AboutPage
+DISTPATH="$PWD/dist"
+cd YTMediaTool || exit
+pyinstaller -F Main.py --distpath "$DISTPATH" --noconsole --name YTMediaTool --hidden-import yt_dlp --hidden-import BasicPage --hidden-import SMLDpage --hidden-import SettingsPage --hidden-import AboutPage
+cd ..
 cp LICENSE dist/LICENSE # copy license to distribution
 xdg-open dist
