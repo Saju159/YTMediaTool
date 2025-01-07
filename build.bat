579823e22b91d73979bbba7ce2784ef689a50b4f
arttu <arttuc@pm.me>
2025-01-07 19:33:31 +0200

    Adjust windows build script to work with new path

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
-pyinstaller -F Main.py --noconsole --name YTMediaTool --hidden-import yt_dlp --hidden-import BasicPage --hidden-import SMLDpage --hidden-import SettingsPage --hidden-import AboutPage
+set distpath=%CD%\dist
+cd YTMediaTool
+pyinstaller -F Main.py --distpath "%distpath%" --noconsole --name YTMediaTool --hidden-import yt_dlp --hidden-import BasicPage --hidden-import SMLDpage --hidden-import SettingsPage --hidden-import AboutPage
+cd ..
 copy "LICENSE" "dist\LICENSE.txt"
 explorer dist
