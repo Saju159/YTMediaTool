d54dd8bdd7d0be114711ad864b6e0f81a65e91e9
Saju159 <92033581+Saju159@users.noreply.github.com>
2026-01-09 10:41:46 +0200

    yt music api test part 2

--- YTMediaTool/yttest.py
+++ YTMediaTool/yttest.py
@@ -7,8 +7,8 @@ def extract_playlist_id(playlist_url):
     match = re.search(pattern, playlist_url)
     return match.group(0) if match else None
 
-def get_playlist_details(playlist_url):
-    """Retrieves song, artist, and album information from the playlist."""
+def ytmusicurl(playlist_url):
+    #"""Retrieves song, artist, and album information from the playlist."""
     ytmusic = YTMusic()
     playlist_id = extract_playlist_id(playlist_url)
     if not playlist_id:
@@ -17,7 +17,7 @@ def get_playlist_details(playlist_url):
 
     # Fetch playlist items
     playlist_items = ytmusic.get_playlist(playlist_id)['tracks']
-    
+
     if not playlist_items:
         print("No songs found in the playlist.")
         return
@@ -26,14 +26,14 @@ def get_playlist_details(playlist_url):
         # Print the details of each song
         for track in range(len(playlist_items)):
             #print(str(playlist_items[track]) + "\n")
-            video_id = playlist_items[track]['videoId']
-            song_title = playlist_items[track]['title']
+            video_id = playlist_items[track]['videoId'].strip()
+            song_title = playlist_items[track]['title'].strip()
             artist = playlist_items[track]['artists']
             album = playlist_items[track]['album']
 
-            artist = str(artist).split(":")[1].split(",")[0].replace("'", "")
+            artist = str(artist).split(":")[1].split(",")[0].replace("'", "").strip()
             if not album == None:
-                album = str(album).split(":")[1].split(",")[0].replace("'", "")
+                album = str(album).split(":")[1].split(",")[0].replace("'", "").strip()
             else:
                 album = "[Unkown Album]"
             
@@ -44,4 +44,4 @@ def get_playlist_details(playlist_url):
 
 if __name__ == "__main__":
     playlist_url = "https://music.youtube.com/playlist?list=PLjCT52YyPOMTyaAfVNB0DLbJYON2rAkbh" #input("Enter the YouTube Music playlist URL: ")
-    get_playlist_details(playlist_url)
+    ytmusicurl(playlist_url)
