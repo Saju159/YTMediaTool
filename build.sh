d0cf690efeedf29501ccfa2299cab183b74a7990
Arttu <arttuc@pm.me>
2026-01-21 11:30:34 +0200

    Remove linux build.sh
    Linux builds don't use pyinstaller anymore.

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-DISTPATH="$PWD/dist"
-cd YTMediaTool || exit
-pyinstaller -F Main.py --distpath "$DISTPATH" --noconsole --name YTMediaTool --hidden-import yt_dlp --hidden-import BasicPage --hidden-import SMLDpage --hidden-import SettingsPage --hidden-import AboutPage
-cd ..
-cp LICENSE dist/LICENSE # copy license to distribution
-xdg-open dist
